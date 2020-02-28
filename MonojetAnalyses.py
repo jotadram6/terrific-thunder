906c018af81c587ba3b981db6b001f6087b40510
Jose Ruiz <jotadram6@gmail.com>
2020-02-28 11:50:12 -0500

    Monojet results

--- MonojetAnalyses.py
+++ MonojetAnalyses.py
@@ -10,18 +10,18 @@ if args.ANA=="CMS Monojet":
     METhisto = ROOT.TH1F("MET","MET",20,200,2000)
 
     for entry in xrange(InitialEvts):
-        TreeRelader.ReadEntry(entry)
+        TreeReader.ReadEntry(entry)
 
         NJets=Np(Branches["Jet"])
         
-        if Branches["MissingET"].At(0).MET<=200: continue
+        if Branches["Met"].At(0).MET<=200: continue
         if NJets < 1: continue
         if PT(Branches["Jet"],0)<100 or abs(ETA(Branches["Jet"],0))>2.5: continue
 
-        Dp1=DeltaPhi(PHI(Branches["Jet"],0),PHI(Branches["MissingET"],0))
-        if branchJet.GetEntries()>=2: Dp2=DeltaPhi(PHI(Branches["Jet"],1),PHI(Branches["MissingET"],1))
-        if branchJet.GetEntries()>=3: Dp3=DeltaPhi(PHI(Branches["Jet"],2),PHI(Branches["MissingET"],2))
-        if branchJet.GetEntries()>=4: Dp4=DeltaPhi(PHI(Branches["Jet"],3),PHI(Branches["MissingET"],3))
+        Dp1=DeltaPhi(PHI(Branches["Jet"],0),PHI(Branches["Met"],0))
+        if NJets>=2: Dp2=DeltaPhi(PHI(Branches["Jet"],1),PHI(Branches["Met"],0))
+        if NJets>=3: Dp3=DeltaPhi(PHI(Branches["Jet"],2),PHI(Branches["Met"],0))
+        if NJets>=4: Dp4=DeltaPhi(PHI(Branches["Jet"],3),PHI(Branches["Met"],0))
 
         #Tau veto and recalculation of HT
         NTau=0
@@ -40,8 +40,8 @@ if args.ANA=="CMS Monojet":
         #Muon veto
         if not Veto(Branches["Muon"],PTCut=10,ETACut=2.4): continue
 
-        if Branches["MissingET"].At(0).MET>float(args.MET):
-        CountingEvents=CountingEvents+1
-        METhisto.Fill(Branches["MissingET"].At(0).MET)
+        if Branches["Met"].At(0).MET>float(args.MET):
+            CountingEvents=CountingEvents+1
+            METhisto.Fill(Branches["Met"].At(0).MET)
 
     DisplayFinalInfo(InitialEvts, CountingEvents, Histo=METhisto, MyFile=RootFile)
